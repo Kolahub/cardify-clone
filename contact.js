a0b30ce36a9d5f46e5f3700c5e6b69d0a2e821d7
Ayofe Faheez <ayofefaheez@gmail.com>
2024-02-29 14:54:55 -0800

    updated

--- contact.js
+++ contact.js
@@ -1,3 +1,18 @@
-var map = L.map('map').setView([7.3870801,3.822658], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-L.marker([51.5, -0.09]).addTo(map);
+var map = L.map('map').setView([0, 0], 13);
+L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png').addTo(map);
+
+  // Use geolocation to get the user's current position
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    // Create a marker at the user's current position
+    var marker = L.marker([latitude, longitude]).addTo(map);
+    marker.bindPopup("You are here!").openPopup();
+
+    // Center the map on the user's location
+    map.setView([latitude, longitude], 16);
+  }, function(error) {
+    console.error("Error getting user location:", error);
+    alert("Error getting user location. Please allow location access.");
+  });
