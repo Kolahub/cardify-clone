56920b596df70189c91ad40e0f4eebfdb5c7ba83
Ayofe Faheez <ayofefaheez@gmail.com>
2024-03-04 17:27:36 -0800

    minor changes on the infotips and dropdown component

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 const listBtns = document.querySelectorAll(".navigation__lists");
-const infotipsOverlay = document.querySelectorAll('.infotips__overlay')
-
-listBtns.forEach((infotips) =>
-  infotips.addEventListener("click", function () {
-    infotips.classList.toggle("activeInfo");
-    infotipsOverlay.addEventListener('click', function() {
-      infotips.classList.remove("activeInfo");
-  })
-  })
-);
-
 listBtns.forEach((listBtn) => {
   listBtn.addEventListener("click", function () {
     listBtns.forEach(function (item) {
       if (item !== listBtn) {
         item.classList.remove("active");
+        item.classList.remove("activeInfo");
       }
     });
-    console.log("hello world");
     listBtn.classList.toggle("active");
+    listBtn.classList.toggle("activeInfo");
   });
 });
